3a28b5050d7a0523c5db2657f9a03f9e0dd0fd95
ub4784-creator <ub4784@srmist.edu.in>
2026-02-17 11:14:41 +0530

    this is my UC 2 code

---
+++ UC2Program.java
@@ -0,0 +1,13 @@
+public class UC2Program {
+
+    public static void main(String[] args) {
+
+        System.out.println("  ***      ***      *****      ***** ");
+        System.out.println(" **   **   ** **    **   **    **   **");
+        System.out.println(" **   **  **   **    **   **    **   **");
+        System.out.println(" **   **  **   **    *****      ***** ");
+        System.out.println(" **   **  ********    **         **   ");
+        System.out.println(" **   **  **     **    **         **   ");
+        System.out.println("  ***     **     **    **         *****");
+    }
+}
