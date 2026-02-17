b01144dcf581365668558526c4ee1add5a1960df
ub4784-creator <ub4784@srmist.edu.in>
2026-02-17 10:13:36 +0530

    this is my UC code

--- UC1Programs.java
+++ UC1Programs.java
@@ -1,6 +1,6 @@
 public  class UC1Programs {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+        System.out.println("OOPS");
     }
 
 }
